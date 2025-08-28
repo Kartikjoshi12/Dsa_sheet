1ecf31ff3c486dac986c5c741697c7af4e788c15
kartik joshi <kartikjoshi2208@gmail.com>
2025-08-28 12:40:03 +0530

    new v3eector

--- stl/veector.cpp
+++ stl/veector.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+
     vector<pair<int,int>> arr = {{1,2}, {2,3}, {3,4}}; 
 
     arr.push_back({4,5}); 
@@ -11,10 +12,57 @@ int main() {
 
     arr.emplace_back(5,6); 
 
+
+// --------------------iterator in cppp in vectors-----------------
     vector<pair<int,int>>::iterator pr = arr.end();
     pr--; 
-
     cout << pr->first << " " << pr->second << endl; 
 
+
+// with out pair 
+
+    vector<int> arr1 = {1,2,3,4,5,6};
+
+    // iterator with begin
+    auto pr2 = arr1.begin();
+    pr2++;
+    cout << *pr2 << endl;   // prints 2
+
+    // iterator with end
+    auto pr3 = arr1.end();
+    pr3--;                  // move one step back (now points to last element)
+    cout << *pr3 << endl;   // prints 6
+
+    // [] works with iterator
+    cout << pr2[2] << endl; // prints 4 (since pr2 points at 2 → +2 offset)
+
+    // .at() works only on vector
+    cout << arr1.at(3) << endl; // prints 4
+
+    // .back() works only on vector
+    cout << arr1.back() << endl; // prints 6
+
+// ----------now for loop on vector-------------------
+
+ for(auto it = arr1.begin();it!= arr1.end(); it++){
+cout<< *it<<" ";
+ }
+cout<<endl;
+ for(auto it : arr1){
+cout<< it<<" ";
+ }
+cout<<endl;
+// erase method in vector
+
+arr1.erase(arr1.begin()+2);
+ for(auto it : arr1){
+cout<< it<<" ";
+ }
+  cout<<endl;
+arr1.erase(arr1.end()-1);
+for(auto it : arr1){
+cout<< it<<" ";
+ }
+ cout<<endl;
     return 0;
 }
