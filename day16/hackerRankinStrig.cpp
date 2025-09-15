8649ea09d727b21b7b903b3a98cbabb81c8313f5
kartik joshi <kartikjoshi2208@gmail.com>
2025-09-16 00:09:49 +0530

    new q

---
+++ day16/hackerRankinStrig.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string hackerrankInString(string s) {
+    string target = "hackerrank";
+    int j = 0; // pointer for target
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == target[j]) {
+            j++;
+        }
+        if (j == target.size()) {
+            return "YES"; // matched all letters
+        }
+    }
+    return "NO"; // not matched fully
+}
+
+int main() {
+    int q;
+    cin >> q;
+    while (q--) {
+        string s;
+        cin >> s;
+        cout << hackerrankInString(s) << endl;
+    }
+    return 0;
+}
